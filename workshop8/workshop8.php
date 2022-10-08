67a19d501427b113829fc0fb81503d721c57abfb
Pakawit <s6304062616234@email.kmutnb.ac.th>
2022-10-08 22:34:22 +0700

    1

---
+++ workshop8/workshop8.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="add.php" method="post">
+        username : <input type="text" name="username"><br>
+        pasword : <input type="password" name="password"><br>
+        ชื่อ <input type="text" name="name"><br>
+        ที่อยู่: <br>
+        <textarea name="address" rows="3" cols="40"></textarea><br>
+        มือถือ: <input type="phone" name="mobile"><br>
+        อีเมล: <input type="text" name="email"><br><br>
+        <input type="submit" value="เพิ่มสมาชิก ">
+    </form>
+</body>
+</html>
